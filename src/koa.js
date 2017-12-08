38eaeb0204010994ae0850318a705426b56af717
吳東曄 Wu, Dung-Ie <tw0517tw@gmail.com>
2017-12-08 12:38:10 +0800

    return next

--- src/koa.js
+++ src/koa.js
@@ -10,6 +10,6 @@ export default function botanalyticsMiddleware(bot, { apiKey }) {
   return ({ request }, next) => {
     botanalytics.logIncomingMessage(request.body);
 
-    next();
+    return next();
   };
 }
